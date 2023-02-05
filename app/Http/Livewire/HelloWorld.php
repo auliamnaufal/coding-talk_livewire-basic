cb316960f580e983061cb3223f46be78b4319cde
auliamnaufal <auliamnaufal@gmail.com>
2023-02-05 15:49:56 +0800

    feat: bind checkbox to a load variable to show "!" everytime the box is checked

--- app/Http/Livewire/HelloWorld.php
+++ app/Http/Livewire/HelloWorld.php
@@ -7,6 +7,7 @@ use Livewire\Component;
 class HelloWorld extends Component
 {
     public $name = 'Naufal';
+    public $loud = false;
 
     public function render()
     {
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,6 +1,7 @@
 <div>
     <input type="text" wire:model="name">
+    <input type="checkbox" wire:model="loud">
 
-    Hello {{ $name }}
+    Hello {{ $name }} @if($loud) ! @endif
 </div>
 
